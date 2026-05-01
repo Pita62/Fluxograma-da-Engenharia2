8e59cf8adcaa5e928b1bde7fd3090c36c31a0dc5
Pita62 <guaru.guerreiro@gmail.com>
2026-05-01 16:51:58 -0300

    Novo exercício, de Epitácio, com códigos difer.:Fluxogramada Engenharia.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Faça um programa que implemente o meme Fluxograma da Engenharia .");
+Console.WriteLine("--- Fluxograma Da Engenharia ---");
+Console.WriteLine();
+
+Console.Write("O objeto está se movendo (S/N)? ");
+string respostaMovendo = Console.ReadLine()!.ToUpper();
+
+Console.Write("Deveria (S/N)? ");
+string respostaDeveria = Console.ReadLine()!.ToUpper();
+
+Console.WriteLine();
+
+if (respostaMovendo == "N")
+{
+    if (respostaDeveria == "N")
+    {
+        Console.WriteLine("Ótimo!");
+    }
+    else
+    {
+        Console.WriteLine("Use WD-40.");
+    }
+}
+else
+{
+    if (respostaDeveria == "S")
+    {
+        Console.WriteLine("Ótimo!");
+    }
+    else
+    {
+        Console.WriteLine("Use Silver Tape.");
+    }
+}
